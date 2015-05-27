84f72fe1ae052506b904ce6d0038765bd93abb90
Gavin Delphia <magicgavin2004@gmail.com>
2015-05-27 11:47:26 -0500

    Screenshot update

---
+++ settings.js
@@ -0,0 +1,15 @@
+var settings = {
+	"leagueInstallDir": "C:\\Riot Games",
+	"garenaInstallDir": "C:\\Program Files (x86)\\GarenaLoL",
+	"syncMode": false,
+	"showFrequent": false,
+	"showHighWin": true,
+	"showStarters": true,
+	"showConsumables": true,
+	"showSkillOrder": true,
+	"removeOldSets": true,
+	"starterSkillsToShow": 5,
+	"leagueItemsetDir": "\\League of Legends\\Config\\Champions\\",
+	"garenaItemsetDir": "\\GameData\\Apps\\LoL\\Game\\Config\\Champions\\",
+	"garenaRegion": false
+}
